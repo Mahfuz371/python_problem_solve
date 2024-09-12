e0c3c3de2c89f5ee02478d059840d159db3b52a1
Mahfuz371 <mahfuzl345667@gmail.com>
2024-09-12 19:59:27 +0600

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,15 @@
+x = input("Enter a single character:")
+if x == "a" :
+    character = "vowel"
+elif x == "e" :
+    character = "vowel"
+elif x == "i" :
+    character = "vowel"
+elif x == "o" :
+    character = "vowel"
+elif x == "u" :
+    character = "vowel"
+else:
+  character = "consonent"
+print(f"{x} is {character}")
+
